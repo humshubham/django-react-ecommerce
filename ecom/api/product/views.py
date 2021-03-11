9d507425d67ffcdd3391c767dcb00e7b8f05ecc8
humshubham <shubhams12333@gmail.com>
2021-03-11 15:52:29 +0530

    added products

--- ecom/api/product/views.py
+++ ecom/api/product/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Product
+from .serialize import ProductSerialzer
 # Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('name')
+    serializer_class = ProductSerialzer
--- ecom/api/urls.py
+++ ecom/api/urls.py
@@ -4,7 +4,8 @@ from .views import home
 
 urlpatterns = [
     path("", home, name="api.home"),
-    path("category/", include('api.category.urls'))
+    path("category/", include('api.category.urls')),
+    path("product/", include('api.product.urls'))
 
 
 ]
